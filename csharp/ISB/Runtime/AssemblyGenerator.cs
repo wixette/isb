44e058fc4177c1121e5ae72f9d653b404d51bb4e
wixette <wixette@gmail.com>
2021-02-23 23:26:07 +0800

    Trivial update for AssemblyGenerator.

--- csharp/ISB/Runtime/AssemblyGenerator.cs
+++ csharp/ISB/Runtime/AssemblyGenerator.cs
@@ -1,14 +1,20 @@
 // This is a derived work of Microsoft Small Basic (https://github.com/sb).
 // The original code is licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 
 using ISB.Parsing;
+using ISB.Utilities;
 
 namespace ISB.Runtime
 {
     public sealed class AssemblyGenerator
     {
+        private readonly DiagnosticBag diagnostics;
+        private readonly string moduleName;
+        private int instructionIndex;
+
         public Assembly AssemblyBlock { get; private set; }
 
         public AssemblyGenerator(string moduleName,
@@ -16,6 +22,8 @@ namespace ISB.Runtime
             IDictionary<string, int> labelDictionary,
             int startInstructionNo = 0)
         {
+            this.moduleName = moduleName;
+            this.instructionIndex = Math.Max(0, startInstructionNo);
             this.AssemblyBlock = new Assembly();
         }
     }
