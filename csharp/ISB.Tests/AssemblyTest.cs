0943e009223078fa32a21c409d7c357f361d84cd
Yonggang Wang <wixette@gmail.com>
2021-02-26 16:50:14 +0800

    Adds more tests for Assembly parsing.

--- csharp/ISB.Tests/AssemblyTest.cs
+++ csharp/ISB.Tests/AssemblyTest.cs
@@ -34,10 +34,25 @@ label3:
 
             Assembly assembly2 = Assembly.Parse(asm);
             Assert.Equal(8, assembly2.Instructions.Count);
+
             Assert.Equal(Instruction.NOP, assembly2.Instructions[0].Name);
+            Assert.Equal(Instruction.ADD, assembly2.Instructions[1].Name);
+            Assert.Equal(Instruction.SUB, assembly2.Instructions[2].Name);
+            Assert.Equal(Instruction.BR, assembly2.Instructions[3].Name);
+            Assert.Equal(Instruction.BR_IF, assembly2.Instructions[4].Name);
+            Assert.Equal(Instruction.STORE_ARR, assembly2.Instructions[5].Name);
+            Assert.Equal(Instruction.PUSH, assembly2.Instructions[6].Name);
+            Assert.Equal(Instruction.PUSHS, assembly2.Instructions[7].Name);
+
             Assert.Equal("label1", assembly2.Instructions[1].Label);
+            Assert.Equal("label2", assembly2.Instructions[4].Oprand1.ToString());
             Assert.Equal("label3", assembly2.Instructions[4].Oprand2.ToString());
+
+            Assert.True(assembly2.Instructions[5].Oprand1 is StringValue);
+            Assert.Equal("a", assembly2.Instructions[5].Oprand1.ToString());
+            Assert.True(assembly2.Instructions[5].Oprand2 is NumberValue);
             Assert.Equal("2", assembly2.Instructions[5].Oprand2.ToString());
+
             Assert.Equal("Say \"Hello, World!\"", assembly2.Instructions[7].Oprand1.ToString());
         }
     }
