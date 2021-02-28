6052ae4286eebe6da2f9adbef47f72d31c571c43
wixette <wixette@gmail.com>
2021-03-01 01:01:38 +0800

    Parses commandline arguments.

--- csharp/ISB.Shell/Program.cs
+++ csharp/ISB.Shell/Program.cs
@@ -1,17 +1,74 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Reflection;
+using System.Collections;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
 
 namespace ISB.Shell
 {
     class Program
     {
+        public class Options
+        {
+            [Option('i', "input", Required=false,
+                HelpText="ISB source (*.bas) file or ISB assembly (*.asm) file to run. " +
+                    "If not set, the interactive shell mode will be on.")]
+            public string InputFile { get; set; }
+
+            [Option('c', "compile", Required=false,
+                HelpText="Compile ISB source to ISB assembly.")]
+            public bool Compile { get; set; }
+
+            [Option('o', "output", Required=false,
+                HelpText="Output ISB assembly file when --compile is set. " +
+                    "If not set, the assembly will be written to stdout.")]
+            public string OutputFile { get; set; }
+        }
+
+        private const string BasicExtension = ".bas";
+        private const string AssemblyExtension = ".asm";
+
         static void Main(string[] args)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            Console.WriteLine($"{fvi.ProductName}, v{fvi.ProductVersion}, {fvi.LegalCopyright}");
-            Console.WriteLine("Welcome");
+            var parserResult = Parser.Default.ParseArguments<Options>(args);
+            var helpText = HelpText.AutoBuild<Options>(parserResult, h => h, e => e);
+            parserResult.WithParsed(options =>
+            {
+                Console.Error.WriteLine(helpText.Heading);
+                Console.Error.WriteLine(helpText.Copyright);
+                RunOptions(options);
+            });
+        }
+
+        static void RunOptions(Options opts)
+        {
+            if (!String.IsNullOrWhiteSpace(opts.InputFile))
+            {
+                string ext = Path.GetExtension(opts.InputFile);
+                if (ext != null && ext.ToLower() == BasicExtension)
+                {
+                    if (opts.Compile)
+                    {
+                        Console.Error.WriteLine($"Compiling {opts.InputFile} to assembly code.");
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"Running BASIC program {opts.InputFile}.");
+                    }
+                }
+                else if (ext != null && ext.ToLower() == AssemblyExtension)
+                {
+                    Console.Error.WriteLine($"Running ISB Assembly program {opts.InputFile}.");
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Only {BasicExtension} or {AssemblyExtension} file is supported.");
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Entering the interactive mode.");
+            }
         }
     }
 }
