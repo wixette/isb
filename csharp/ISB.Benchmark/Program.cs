2d2a013cc6294f1fef2368633b274faf6e2c22c5
wixette <wixette@gmail.com>
2021-10-10 13:54:57 +0800

    Add a new benchmark

--- csharp/ISB.Benchmark/Program.cs
+++ csharp/ISB.Benchmark/Program.cs
@@ -7,10 +7,38 @@ namespace ISB.Benchmark
 {
     public class CompilerAndInterpreterBenchmark
     {
+        private const string PrimeCheckingCode = @"n = 1000117 ' number to be test.
+IsPrime = 0
+if n <= 3 then
+  if n > 1 then
+    IsPrime = 1
+    goto TheEnd
+  else
+    IsPrime = 0
+    goto TheEnd
+  endif
+elseif n mod 2 = 0 or n mod 3 = 0 then
+  IsPrime = 0
+  goto TheEnd
+else
+  i = 5
+  while i * i <= n
+    if n mod i = 0 or n mod (i + 2) = 0 then
+      IsPrime = 0
+      goto TheEnd
+    endif
+    i = i + 6
+  endwhile
+  IsPrime = 1
+endif
+TheEnd:
+";
+
         private readonly Random _random = new Random();
         private readonly Engine _engineForCompile = new Engine("BenchmarkCompile");
         private readonly Engine _engineForRun = new Engine("BenchmarkRun1");
         private readonly Engine _engineForRunWithLibCall = new Engine("BenchmarkRun2");
+        private readonly Engine _engineForPrimeChecking = new Engine("BenchmarkRun3");
 
         public CompilerAndInterpreterBenchmark() {
             string code = GenerateCode();
@@ -21,6 +49,10 @@ namespace ISB.Benchmark
             if (!_engineForRunWithLibCall.Compile(codeWithLibCall, true)) {
                 throw new Exception(
                     _engineForRunWithLibCall.ErrorInfo.Contents[0].ToDisplayString());
+            }
+            if (!_engineForPrimeChecking.Compile(PrimeCheckingCode, true)) {
+                throw new Exception(
+                    _engineForPrimeChecking.ErrorInfo.Contents[0].ToDisplayString());
             }
         }
 
@@ -48,6 +80,15 @@ namespace ISB.Benchmark
                     _engineForRunWithLibCall.ErrorInfo.Contents[0].ToDisplayString());
             }
             return _engineForRunWithLibCall.StackCount;
+        }
+
+        [Benchmark]
+        public int RunPrimeChecking() {
+            if (!_engineForPrimeChecking.Run(true)) {
+                throw new Exception(
+                    _engineForPrimeChecking.ErrorInfo.Contents[0].ToDisplayString());
+            }
+            return _engineForPrimeChecking.StackCount;
         }
 
         private string GenerateCode() {
